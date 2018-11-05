a0fee229a4cec7d3cad7471d10d010ff8c5f3818
Juan <juan.alvarez@escuelaestech.es>
2018-11-05 12:07:00 +0100

    Ejercicio 2 de orientado a objetos en PROCESO

--- src/es/JuanAntonio/Main.java
+++ src/es/JuanAntonio/Main.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Cuenta cuenta = new Cuenta("Antonio", 100);
-         cuenta.retirar(30);
-        System.out.println(cuenta.getCantidad());
 
 
     }
